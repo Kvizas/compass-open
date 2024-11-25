1651357bfab878c84e25a50b82ba2a46df627c0c
Kvizas <theqxizgaming@gmail.com>
2024-11-25 14:21:44 +0200

    remove unused pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,9 @@ export default function Page() {
   return (
     <div>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
-        <meta
-          name="og:image"
-          content={`${
-            process.env.NEXT_PUBLIC_VERCEL_URL
-              ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
-              : ""
-          }/api/vercel`}
-        />
+        <meta name="og:title" content="Compass" />
       </Head>
-      <h1>
-        A page with Open Graph Image. url: {process.env.NEXT_PUBLIC_VERCEL_URL}
-      </h1>
+      <h1>What are you looking for?</h1>
     </div>
   );
 }
